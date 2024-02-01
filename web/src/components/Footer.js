88881272c7fc8a429e4950f06f11278bc08a913d
SunnyGPT <126902077+SunnyGPT@users.noreply.github.com>
2024-02-02 01:36:33 +0800

    Update Footer.js

--- web/src/components/Footer.js
+++ web/src/components/Footer.js
@@ -37,25 +37,7 @@ const Footer = () => {
           ></div>
         ) : (
           <div className='custom-footer'>
-            <a
-              href='https://github.com/Calcium-Ion/new-api'
-              target='_blank'
-            >
-              New API {process.env.REACT_APP_VERSION}{' '}
-            </a>
-            由{' '}
-            <a href='https://github.com/Calcium-Ion' target='_blank'>
-              Calcium-Ion
-            </a>{' '}
-            开发，基于{' '}
-            <a href='https://github.com/songquanpeng/one-api' target='_blank'>
-              One API v0.5.4
-            </a>{' '}
-            ，本项目根据{' '}
-            <a href='https://opensource.org/licenses/mit-license.php'>
-              MIT 许可证
-            </a>{' '}
-            授权
+© 2023 OPENAI365 Corp.
           </div>
         )}
       </Layout.Content>
